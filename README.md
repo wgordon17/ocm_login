d8cdd349bf7079e2f41d6964ce3e7f0be048323f
Will Gordon <wgordon@redhat.com>
2024-05-28 15:26:18 -0400

    Cleans up external SSO usage Interactively prompts for password if excluded from terminal Closes #1, closes #2

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # Automated headless authentication to OpenShift Cluster Manager
 
+## Installation
+
+Installation assumes that you already have Python3 on your machine.
+
+* Install [PDM](https://pdm-project.org/latest/#installation)
+* Install python dependencies
+    ```bash
+    $ pdm install
+    ```
+* Using `pdm` to run the rest of the commands:
+    ```bash
+    $ pdm run playwright install chromium
+    $ pdm run src/ocm_login/main.py --user <username> ...
+    ```
+
 ## Usage
 
 Supports logging in with a standard username/password combination
 
 ```bash
-$ python main.py --user <username> --pass <password> [--url <ocm backend api URL>]
+$ pdm run src/ocm_login/main.py --user <username> --password <password> [--ocm-url https://api.openshift.com]
 ```
 
 Also supports logging in to OCM with a redirect to an SSO endpoint
 
 ```bash
-$ python main.py --user <ocm_username> --second-user <sso_username> --pass <sso_password>
+$ pdm run src/ocm_login/main.py -u <ocm_username> --sso-user <sso_username> --sso-url <sso-url-endpoint> -p <sso_password>
 ```
+
+### Example
+
+```bash
+$ pdm run src/ocm_login/main.py --user admin.openshift --sso-user admin@redhat.com --sso-url auth.redhat.com
+```
+
+Excluding `--password` will interactively prompt the user for their password
--- src/ocm_login/main.py
+++ src/ocm_login/main.py
@@ -1,12 +1,14 @@
 import argparse
 import re
+import sys
+from getpass import getpass
 
 import sh
 from playwright.sync_api import sync_playwright
 
 
 def ocm_login():
-    process = sh.ocm("login", "--use-device-code", "--url", args.url, _out=login_sso, _bg=True)
+    process = sh.ocm("login", "--use-device-code", "--url", args.ocm_url, _out=login_sso, _bg=True)
     process.wait()
     print(f"Logged in as: {sh.jq("-r", ".username", _in=sh.ocm("whoami", _piped=True))}")
 
@@ -34,18 +36,25 @@ def login_sso(output):
     page.locator("#username-verification").fill(args.user)
     page.get_by_role("button", name="Next").click()
 
-    if args.second_user:
-        page.wait_for_url("https://auth.redhat.com/auth/realms/EmployeeIDP/login-actions/authenticate**")
-        page.get_by_placeholder("KERBEROS ID").fill(args.second_user)
+    # Prompt for password if not provided on command line
+    if not args.password:
+        args.password = getpass()
+
+    if args.sso_url:
+        page.wait_for_url(re.compile(rf"(https://)?{args.sso_url}.*"), wait_until="domcontentloaded")
+        # These locators may not be the same for all SSO endpoints, PR's welcome
+        page.get_by_placeholder("KERBEROS ID").fill(args.sso_user)
         page.get_by_role("textbox", name="password").fill(args.password)
         page.get_by_role("button", name="Log in").click()
 
     else:
+        page.wait_for_url("https://sso.redhat.com/auth/realms/redhat-external/login-actions/authenticate**")
         password = page.get_by_role("textbox", name="password")
         password.wait_for()
         password.fill(args.password)
         page.get_by_role("button", name="Log in").click()
 
+    # Assume authentication was successful, and grant access
     page.wait_for_url("https://sso.redhat.com/auth/realms/redhat-external/login-actions/required-action**")
     page.get_by_role("button", name="Grant access").click()
     browser.close()
@@ -56,11 +65,26 @@ def login_sso(output):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--user")
-    parser.add_argument("--second-user")
-    parser.add_argument("--password")
-    parser.add_argument("--url", default="prod")
+    parser = argparse.ArgumentParser(description="Automated headless authentication to OpenShift Cluster Manager (OCM)")
+    parser.add_argument("--user", "-u", help="OCM username", required=True)
+    sso_user = parser.add_argument("--sso-user", help="SSO username (optional)")
+    sso_url = parser.add_argument("--sso-url", help="SSO URL (optional), e.g., https://sso.redhat.com")
+    parser.add_argument(
+        "--password",
+        "-p",
+        "--pass",
+        help="OCM password, or SSO password if --sso-user is specified. Interactively prompts for password if not supplied",
+    )
+    parser.add_argument("--ocm-url", "--url", default="prod", help="OCM URL, default: prod (https://api.openshift.com)")
     args = parser.parse_args()
+
+    sso_args = [args.sso_user, args.sso_url]
+    if any(sso_args) and not all(sso_args):
+        parser.print_usage(sys.stderr)
+        print()
+        if not args.sso_user:
+            raise SystemExit(argparse.ArgumentError(sso_user, "also requires --sso-url"))
+        elif not args.sso_url:
+            raise SystemExit(argparse.ArgumentError(sso_url, "also requires --sso-user"))
 
     ocm_login()
